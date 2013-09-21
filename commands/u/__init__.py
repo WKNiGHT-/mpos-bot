d738cc2cb60d29ff45571a0404377c97088f5bc7
Sebastian Grewe <sebastian@grewe.ca>
2013-09-21 02:20:55 +0200

    [FIX] Fixing crash when missing username

--- commands/u/__init__.py
+++ commands/u/__init__.py
@@ -9,5 +9,6 @@ def run(line, config):
     if len(userNick) == 2:
         userNick = userNick[1]
         url = urllib.urlopen(config['api_url'] + '&action=getuserstatus&api_key=' + config['api_key'] + '&id=' + userNick)
-    jsonData = json.loads(url.read())
-    return 'PRIVMSG ' + config['channel'] + ' :' + 'Username: ' + str(jsonData['getuserstatus']['data']['username']) + ' | Hashrate: ' + str(jsonData['getuserstatus']['data']['hashrate']) + ' kh/s' + ' | Shares Valid: ' + str(jsonData['getuserstatus']['data']['shares']['valid']) + ' | Shares Invalid: ' + str(jsonData['getuserstatus']['data']['shares']['invalid'])
+        jsonData = json.loads(url.read())
+        return 'PRIVMSG ' + config['channel'] + ' :' + 'Username: ' + str(jsonData['getuserstatus']['data']['username']) + ' | Hashrate: ' + str(jsonData['getuserstatus']['data']['hashrate']) + ' kh/s' + ' | Shares Valid: ' + str(jsonData['getuserstatus']['data']['shares']['valid']) + ' | Shares Invalid: ' + str(jsonData['getuserstatus']['data']['shares']['invalid'])
+    return 'PRIVMSG ' + config['channel'] + ' : Unable to find username'
