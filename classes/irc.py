7406770ea68b6b4e1ee06ff21f26674f0694f439
Sebastian Grewe <sebastian@grewe.ca>
2013-09-21 10:08:23 +0200

    [LOGGING] Added logging of IRC commands

--- classes/irc.py
+++ classes/irc.py
@@ -9,6 +9,7 @@ class IRC:
         self.irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
         self.irc.connect((host, port ))
     def send(self, message):
+        self.logger.debug(message)
         self.irc.send(message + '\n')
     def recv(self, size):
         return self.irc.recv(size).strip()
