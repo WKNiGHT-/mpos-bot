848ce0a2467558a11204efed348c91ecfb68e319
Sebastian Grewe <sebastian@grewe.ca>
2013-09-21 23:59:06 +0200

    Overhauled socket connection handling
    * [IMPROVED] Use non-blocking sockets
    * [IMPROVED] Moved generic calls into IRC Class
    * [IMPROVED] Created settings module to translate config to settings
    * [IMPROVED] Error handling via logging in main while loop
    
    Known issue: If the socket dies, the bot will not be able to detect it
    yet. I am unsure how this would walk using select.select().

---
+++ lib/settings.py
@@ -0,0 +1,12 @@
+# Convert our config file
+def load(config):
+    settings = {}
+    settings['host'] = config.get('IRC', 'host')
+    settings['port'] = config.getint('IRC', 'port')
+    settings['nick'] = config.get('IRC', 'nick')
+    settings['user'] = config.get('IRC', 'user')
+    settings['channel'] = config.get('IRC', 'channel')
+    settings['interval'] = config.getint('Blockupdate', 'interval')
+    settings['api_key'] = config.get('API', 'key')
+    settings['api_url'] = config.get('API', 'url')
+    return settings
